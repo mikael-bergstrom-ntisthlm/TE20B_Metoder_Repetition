acd5837564c8a8f791a0c1a0ba90c5c7ba247563
Mikael Bergström <krank23@gmail.com>
2022-01-26 10:29:41 +0100

    Initial commit

---
+++ TE20B_Metoder_Repetition/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// Console.WriteLine("Hello");
+
+// string n = Console.ReadLine();
+
+// string n = GetName();
+
+int dmg = Attack(99, 500);
+
+
+
+Console.ReadLine();
+
+
+static string GetName()
+{
+  Console.WriteLine("Skriv ditt namn:");
+  string name = Console.ReadLine();
+
+  return name;
+}
+
+static int Attack(int minDmg, int maxDmg)
+{
+  Random generator = new Random();
+  int damage = generator.Next(minDmg, maxDmg);
+  Console.WriteLine($"You do {damage} damage!");
+
+  return damage;
+}
